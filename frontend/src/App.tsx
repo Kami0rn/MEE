8a1dfb27c30ae5f2c039be1402685d5c00e586dd
Kami0rn <74646193+Kami0rn@users.noreply.github.com>
2024-06-18 23:21:37 +0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Chat from './pages/Chat/Chat';
+import Nav from './pages/Nav/Nav';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Chat></Chat>
+    </div>
+  );
+}
+
+export default App;
